f19e456f9d841fb5f1ac809e8b89f80d7f9d521a
evanstyreke <evanstyreke@protonmail.com>
2021-02-28 11:30:07 -0300

    should be fine

---
+++ OneClick/src/main/java/net/runelite/client/plugins/externals/oneclick/ClickItem.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019-2020, memelorderino <https://github.com/memelorderino>
+ * Copyright (c) 2019, TomC <https://github.com/tomcylke>
+ * All rights reserved.
+ * Licensed under GPL3, see LICENSE for the full scope.
+ */
+package net.runelite.client.plugins.externals.oneclick;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ClickItem
+{
+	private final String name;
+	private final int id;
+}
